905d301729caf0f1dee2b3ea9da601e4c816725b
PatriciaRuffino <sindelformr@gmail.com>
2021-09-06 10:21:37 -0300

    Projeto criado

---
+++ backend/src/test/java/Semana/desenvolvimento/na/semana/Spring/React/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package Semana.desenvolvimento.na.semana.Spring.React;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
